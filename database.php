a32b3ca1522123993ffd0181bb42a721f8c8306d
Smart .u Monday <50284491+smartPYJ@users.noreply.github.com>
2019-08-25 18:30:31 +0100

    Update database.php

--- database.php
+++ database.php
@@ -4,7 +4,7 @@ class Database{
 
     // specify your own database credentials
     private $host = "localhost";
-    private $db_name = "covenant";
+    private $db_name = "DBname";
     private $username = "root";
     private $password = "";
     public $conn;
